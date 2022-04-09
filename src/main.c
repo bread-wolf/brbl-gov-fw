b08dfd0635d52c2389391b4d26bd4f262b86c79d
Augustin-B <augustinbielefeld@gmail.com>
2022-04-09 13:21:21 +0200

    main: Add basic blinky, with clock init code

--- src/main.c
+++ src/main.c
@@ -5,11 +5,22 @@
 
 #include "board.h"
 
+#include "avr/delay.h"
+
 int main(void)
 {
+    // Disable 1/6 clock prescaler
+    CCP = CCP_IOREG_gc;
+    CLKCTRL_MCLKCTRLB = 0;
+
+    // Set LED pin as output
+    LEDBUILTIN_PORT.DIRSET = (1 << LEDBUILTIN_PIN);
+
     for(;;)
     {
-
+        // Blink LED
+        LEDBUILTIN_PORT.OUTTGL = (1 << LEDBUILTIN_PIN);
+        _delay_ms(1000);
     }
 
     return 0;
