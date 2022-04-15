2d2c5302ad7d2b594ac165a03e5a7eacd9264114
Augustin-B <augustinbielefeld@gmail.com>
2022-04-15 16:53:50 +0200

    hal: add general hal include header

---
+++ lib/hal/hal.h
@@ -0,0 +1,15 @@
+/*
+ * Author: Augustin.B
+ * Date  : 15.04.2022
+ */
+
+#ifndef HAL_H_
+#define HAL_H_
+
+// Include helpers.
+#include "helpers/ringBuff.h"
+
+// Include HAL modules.
+#include "serial.h"
+
+#endif /* HAL_H_ */
